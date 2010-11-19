929c8eff6253bdb5f9fb4e7167a2f228c375c611
Oren Goldberg <ogoldberg@gmail.com>
2010-11-19 17:02:25 +0100

    created categories established many-to-many relationship with articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101119143348) do
+ActiveRecord::Schema.define(:version => 20101119155400) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(:version => 20101119143348) do
     t.string   "excerpt"
     t.string   "location"
     t.integer  "user_id"
+  end
+
+  create_table "articles_categories", :id => false, :force => true do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "profiles", :force => true do |t|
