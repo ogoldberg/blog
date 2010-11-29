e43865851138f96e2b3caba164c830f17a688a92
Oren Goldberg <ogoldberg@gmail.com>
2010-11-29 09:18:29 +0100

    sync with tut chap 6 code to ensure integrity

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
+# Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '9a806f5db26cfac308bf9aeeed2cfdecced7752eecc440f5c9b3af15d8b898b03d00f709524b2a52673518f5c5db4ce84d2b91f2f098dff62675c95a5001fe3a'
+Rails.application.config.secret_token = 'ba00674fc07e3be6351f63a2e7a49800b233acfbd2706e6d4f7a477868c3874505025ac253c5f3efd4e6c1e506cbf7075d7245a26a7e3e00aa3f1e52230ff253'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Blog::Application.routes.draw do
+Blog::Application.routes.draw do |map|
   resources :articles
 
   # The priority is based upon order of creation:
@@ -18,12 +18,12 @@ Blog::Application.routes.draw do
   # Sample resource route with options:
   #   resources :products do
   #     member do
-  #       get 'short'
-  #       post 'toggle'
+  #       get :short
+  #       post :toggle
   #     end
   #
   #     collection do
-  #       get 'sold'
+  #       get :sold
   #     end
   #   end
 
@@ -37,7 +37,7 @@ Blog::Application.routes.draw do
   #   resources :products do
   #     resources :comments
   #     resources :sales do
-  #       get 'recent', :on => :collection
+  #       get :recent, :on => :collection
   #     end
   #   end
 
