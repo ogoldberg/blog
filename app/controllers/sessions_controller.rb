63f1f3aa0979a7ef21031dc969a68798a3c28f78
oren <oren@oren-thinkpad.(none)>
2011-01-05 17:06:24 -0600

    authentication filter created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
     def create
-        if user User.authenticate(params[:email], params[:password])
+        if user = User.authenticate(params[:email], params[:password])
             session[:user_id] = user.id
             redirect_to root_path, :notice => "Logged in Successfully"
         else
